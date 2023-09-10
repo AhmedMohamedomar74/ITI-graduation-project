04ef9e771b0714aa3fdadc823dab95730660169e
hagarhassan2001 <126313399+hagarhassan2001@users.noreply.github.com>
2023-09-10 22:14:00 +0300

    Add files via upload

---
+++ HAL/LED/HLED_Confg.h
@@ -0,0 +1,26 @@
+/*
+ * HLED_Confg.h
+ *
+ *  Created on: Sep 10, 2023
+ *      Author: omnia
+ */
+
+#ifndef HAL_HLED_CONFG_H_
+#define HAL_HLED_CONFG_H_
+
+//* LED PORT *//
+#define LED_GRN_PORT     PORTA
+#define LED_BLU_PORT     PORTB
+#define LED_RED_PORT     PORTC
+
+//* LED PIN *//
+#define LED_GRN_PIN  pin6
+#define LED_BLU_PIN  pin6
+#define LED_RED_PIN  pin0
+
+//* LEDS*//
+#define GRN_LED  0
+#define BLU_LED  1
+#define RED_LED  2
+
+#endif /* HAL_HLED_CONFG_H_ */
