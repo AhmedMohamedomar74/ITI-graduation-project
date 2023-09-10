ccb83b9d4ccb6d224e447431b6378b15f86a7373
hagarhassan2001 <126313399+hagarhassan2001@users.noreply.github.com>
2023-09-10 22:13:32 +0300

    Add files via upload

---
+++ HAL/LED/HLED_int.h
@@ -0,0 +1,18 @@
+/*
+ * HLED_int.h
+ *
+ *  Created on: Sep 10, 2023
+ *      Author: omnia
+ */
+
+#ifndef HAL_HLED_INT_H_
+#define HAL_HLED_INT_H_
+#include "stdint.h"
+
+void HLED_void_LEDInit(uint8_t Copy_u8LED);
+void HLED_void_LEDOn(uint8_t Copy_u8LED);
+void HLED_void_LEDOff(uint8_t Copy_u8LED);
+void HLED_void_LEDFlip(uint8_t Copy_u8LED);
+
+
+#endif /* HAL_HLED_INT_H_ */
