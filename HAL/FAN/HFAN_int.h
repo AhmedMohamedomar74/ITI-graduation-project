169e5f6e9cfcc4e94b97e70458065969c3796ddf
hagarhassan2001 <126313399+hagarhassan2001@users.noreply.github.com>
2023-09-10 23:58:16 +0300

    Add files via upload

---
+++ HAL/FAN/HFAN_int.h
@@ -0,0 +1,16 @@
+/*
+ * HFAN_int.h
+ *
+ *  Created on: Sep 10, 2023
+ *      Author: omnia
+ */
+
+#ifndef HAL_FAN_HFAN_INT_H_
+#define HAL_FAN_HFAN_INT_H_
+
+void HFAN_void_FANInit(uint8_t Copy_u8LED);
+void HFAN_void_FANOn(uint8_t Copy_u8LED);
+void HFAN_void_FANOff(uint8_t Copy_u8LED);
+void HFAN_void_FANFlip(uint8_t Copy_u8LED);
+
+#endif /* HAL_FAN_HFAN_INT_H_ */
