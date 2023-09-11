23a7809d937c90df9993319407de6cc3ad952620
RawanMagdyAhmed <144521655+RawanMagdyAhmed@users.noreply.github.com>
2023-09-11 23:15:43 +0300

    Update MCAL_REGS.h

--- MCAL/MCAL_REGS.h
+++ MCAL/MCAL_REGS.h
@@ -54,6 +54,35 @@
 #define  SFIOR_REG       (*((volatile u8*)0x50))
 #define   ADCH_REG       (*((volatile u16*)0x25))
 #define   ADCL_REG       (*((volatile u16*)0x24))
+/********** ADMUX *************/
+#define REFS1_BIT   7
+#define REFS0_BIT   6
+#define ADLAR_BIT   5
+/********** ADCSRA *************/
+#define  ADEN_BIT   7
+#define  ADSC_BIT   6
+#define ADATE_BIT   5
+#define  ADIF_BIT   4
+#define  ADIE_BIT   3
+#define ADPS2_BIT   2
+#define ADPS1_BIT   1
+#define ADPS0_BIT   0
+/*********** SFIOR **************/
+#define ADTS2_BIT  7
+#define ADTS1_BIT  6
+#define ADTS0_BIT  5
+/*********** Voltage ************/
+#define AREF	   0
+#define AVCC       1
+#define RESERVED   2
+#define INTERNAL   3
+
+/*********** MUX_BITS***********/
+#define MUX0_BIT 0
+#define MUX1_BIT 1
+#define MUX2_BIT 2
+#define MUX3_BIT 3
+#define MUX4_BIT 4
 /*
 ************** UART_REG ***************
 */
