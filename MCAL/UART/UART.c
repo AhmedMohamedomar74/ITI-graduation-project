c2aa47b643ed4ab141f03375ca28b7f083ff42e3
Marwa Ahmed <118285095+MarwaRezq@users.noreply.github.com>
2023-09-12 15:02:57 +0300

    Update UART.c
    add this to your main file
    #define F_CPU 1000000UL
    #define UBRR_Val 12

--- MCAL/UART/UART.c
+++ MCAL/UART/UART.c
@@ -1,5 +1,11 @@
-/*************** INCLUDES **************************/
+/*
+ * UART.c
+ *
+ *  Created on: Sep 11, 2023
+ *      Author: Marwa
+ */
 
+/*************** INCLUDES **************************/
 
 #include "UART.h"
 
@@ -7,66 +13,58 @@
  * Driver Modules implementations
  *==============================================================================================================================================*/
 
-/* 
- * Prototype   : void UART_void_Init(u32 baud);
+/*
+ * Prototype   : void UART_void_Init(u16 UBRR_Value);
  * Description : initialize UART with pre configured parameter
- * Arguments   : u32
- * return      : void  
+ * Arguments   : u16
+ * return      : void
  */
- void UART_void_Init(u32 baud)
+ void UART_void_Init(u16 UBRR_Value)
  {
 	/*Choose baud rate that will be used by sender and receiver by writing to UBRRL/UBRRH*/
-	u16 UBRR ;
-	UBRR=(F_CPU/(16*baud))-1 ;
-	UBRRH=(u8)(UBRR>>8);
-	UBRRL=(u8)UBRR;
-	
+
+	UBRRH=(u8)(UBRR_Value>>8);
+	UBRRL=(u8)UBRR_Value;
+
 	/*Enable USART Sender & Receiver*/
-	//SET_BIT(UCSRB,TXEN);
-	SET_BIT(UCSRB,TXEN_BIT);
-	//SET_BIT(UCSRB,RXEN);
-	SET_BIT(UCSRB,RXEN_BIT);
-	
+	SET_BIT(UCSRB,TXEN);
+	SET_BIT(UCSRB,RXEN);
+
 	/*8 bit data to be sent,1 stop bit and no parity bits*/
-	//SET_BIT(UCSRC,URSEL);
-	SET_BIT(UCSRC,URSEL_BIT);
-	//SET_BIT(UCSRC,UCSZ0);
-	SET_BIT(UCSRC,UCSZ0_BIT);
-	//SET_BIT(UCSRC,UCSZ1);
-	SET_BIT(UCSRC,UCSZ1_BIT);
-	
-	
+	SET_BIT(UCSRC,URSEL);
+	SET_BIT(UCSRC,UCSZ0);
+	SET_BIT(UCSRC,UCSZ1);
+
+
  }/*end of UART_void_Init*/
- 
-/* 
+
+/*
  * Prototype   : void UART_void_SendData(u8 data);
  * Description : send 1 byte over UART
  * Arguments   : u8
- * return      : void  
+ * return      : void
  */
  void UART_void_SendData(u8 data)
  {
 	/*Wait for UDR transmit buffer to be empty*/
-	//while(read_BIT(UCSRA,UDRE)==0);
-	while(read_BIT(UCSRA,UDRE_BIT)==0);
+	while(read_BIT(UCSRA,UDRE)==0);
 	/*Put data to UDR transmit buffer transmit*/
-	UDR_R=data ;
- 
+	UDR=data ;
+
  }/*end of UART_void_SendData*/
 
-/* 
+/*
  * Prototype   :  u8 UART_u8_ReceiveData(void);
- * Description : receive 1 byte over UART  
+ * Description : receive 1 byte over UART
  * Arguments   : void
- * return      : u8 received byte  
+ * return      : u8 received byte
  */
  u8 UART_u8_ReceiveData(void)
  {
 	/*Wait for UDR receive buffer to be filled with data*/
-	//while(read_BIT(UCSRA,RXC)==0)
-	while(read_BIT(UCSRA,RXC_BIT)==0);
+	while(read_BIT(UCSRA,RXC)==0);
 	/*Receive data from UDR receive buffer*/
-	return UDR_R ;
-	 
+	return UDR ;
+
  }/*end of UART_u8_ReceiveData*/
 
