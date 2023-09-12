720458fed1ae3455462615680b22953ba23a43fb
hagarhassan2001 <126313399+hagarhassan2001@users.noreply.github.com>
2023-09-12 16:29:28 +0300

    Add files via upload

---
+++ HAL/FAN/HFAN_Confg.h
@@ -0,0 +1,27 @@
+/*
+ * HFAN_Confg.h
+ *
+ *  Created on: Sep 10, 2023
+ *      Author: omnia
+ */
+
+#ifndef HAL_FAN_HFAN_CONFG_H_
+#define HAL_FAN_HFAN_CONFG_H_
+
+
+//* FAN PORT *//
+#define FAN_ONE_PORT       PORTA
+#define FAN_TWO_PORT       PORTB
+#define FAN_THREE_PORT     PORTC
+
+//* FAN PIN *//
+#define FAN_ONE_PIN    GPIO_PIN0
+#define FAN_TWO_PIN    GPIO_PIN0
+#define FAN_THREE_PIN  GPIO_PIN0
+
+//*FANS*//
+#define FAN_ONE     0
+#define FAN_TWO     1
+#define FAN_THREE   2
+
+#endif /* HAL_FAN_HFAN_CONFG_H_ */
